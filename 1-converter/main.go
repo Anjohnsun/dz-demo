9439bb80dcfa3c698c9376c13e2c995f50e8aff3
John <anjohnsun@gmail.com>
2025-11-25 14:30:03 +0300

    homework 1, converter 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -9,8 +9,32 @@ const (
 )
 
 func main() {
-	fmt.Println("Значения констант:")
-	fmt.Println("usd to eur:", usdTOeur)
-	fmt.Println("usd to rub:", usdTOrub)
-	fmt.Println("eur to rub:", eurTorub)
+	n, v1, v2 := SaveInput()
+	Calculate(n, v1, v2)
+}
+
+func SaveInput() (n float64, v1 string, v2 string) {
+	fmt.Print("Введите количество:")
+	_, err := fmt.Scan(&n)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Введите первую валюту(usd, eur, rub):")
+	_, err = fmt.Scan(&v1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Введите вторую валюту(usd, eur, rub):")
+	_, err = fmt.Scan(&v2)
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
+
+func Calculate(n float64, v1 string, v2 string) float64 {
+	return 0
 }
