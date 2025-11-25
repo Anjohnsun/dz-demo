e4f0a4f347ca92f4a1dc3078b64f9681b25239d9
John <anjohnsun@gmail.com>
2025-11-25 16:17:27 +0300

    homework 1, converter 3

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -10,31 +10,74 @@ const (
 
 func main() {
 	n, v1, v2 := SaveInput()
-	Calculate(n, v1, v2)
+	count, currency := Calculate(n, v1, v2)
+	fmt.Println(count, currency)
 }
 
 func SaveInput() (n float64, v1 string, v2 string) {
 	fmt.Print("Введите количество:")
-	_, err := fmt.Scan(&n)
-	if err != nil {
-		panic(err)
+
+	for {
+		_, err := fmt.Scan(&n)
+		if err != nil {
+			fmt.Println("Некорректный ввод, попробуйте ещё раз.")
+			continue
+		}
+		break
 	}
 
-	fmt.Print("Введите первую валюту(usd, eur, rub):")
-	_, err = fmt.Scan(&v1)
-	if err != nil {
-		panic(err)
+	for {
+		fmt.Print("Введите первую валюту(usd, eur, rub):")
+		_, err := fmt.Scan(&v1)
+		if err != nil || !correctCurrencyInput(v1) {
+			fmt.Println("Некорректный ввод, попробуйте ещё раз.")
+			continue
+		}
+		break
 	}
 
-	fmt.Print("Введите вторую валюту(usd, eur, rub):")
-	_, err = fmt.Scan(&v2)
-	if err != nil {
-		panic(err)
+	for {
+		fmt.Print("Введите вторую валюту(usd, eur, rub):")
+		_, err := fmt.Scan(&v2)
+		if err != nil || !correctCurrencyInput(v2) {
+			fmt.Println("Некорректный ввод, попробуйте ещё раз.")
+			continue
+		}
+		break
 	}
-
 	return
 }
 
-func Calculate(n float64, v1 string, v2 string) float64 {
-	return 0
+func correctCurrencyInput(s string) bool {
+	if s != "usd" && s != "eur" && s != "rub" {
+		return false
+	}
+	return true
+}
+
+func Calculate(n float64, v1 string, v2 string) (float64, string) {
+	if v1 == v2 {
+		return n, v2
+	}
+
+	var usdValue float64 = 0
+	switch v1 {
+	case "usd":
+		usdValue = n
+	case "eur":
+		usdValue = n / usdTOeur
+	case "rub":
+		usdValue = n / usdTOrub
+	default:
+		usdValue = 1
+	}
+
+	switch v2 {
+	case "eur":
+		return usdValue * usdTOeur, v2
+	case "rub":
+		return usdValue * usdTOrub, v2
+	default:
+		return usdValue, v2
+	}
 }
